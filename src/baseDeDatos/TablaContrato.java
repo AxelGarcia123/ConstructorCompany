126a21ea5554ee1cb2fdb6ee250197077d398e23
AxelGarcia123/Graphicato <github.com/AxelGarcia123/Graphicato>
2019-11-09 17:37:32 -0600

    Classes tables added

---
+++ src/baseDeDatos/TablaContrato.java
@@ -0,0 +1,33 @@
+package baseDeDatos;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class TablaContrato {
+	private Connection conexion;
+	private java.sql.Statement statement;
+
+	public TablaContrato(Connection conexion) {
+		super();
+		this.conexion = conexion;
+
+		try {
+			statement = conexion.createStatement();
+		} catch (Exception e) {
+			e.toString();
+		} 
+	}
+	
+//	public String guardar(Empleado empleado) {
+//		String sql = "insert into empleado values(null,'" + empleado.getPaterno() + "','" + empleado.getMaterno() + "','" + 
+//				empleado.getNombre() + "','" + empleado.getCargo() + "')";
+//		try {
+//			statement.executeUpdate(sql);
+//			return "Producto registrado";
+//		} catch (SQLException e) {
+//			System.out.println(e.toString());
+//			return sql.toString();
+//		}
+//	}
+}
